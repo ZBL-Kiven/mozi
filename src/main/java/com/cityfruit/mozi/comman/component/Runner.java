e1cf6689d5049432fa272bffb9400b916911d588
杨吉 <284983832@qq.com>
2020-02-27 16:45:47 +0800

    修改部分代码 json 转换

--- src/main/java/com/cityfruit/mozi/comman/component/Runner.java
+++ src/main/java/com/cityfruit/mozi/comman/component/Runner.java
@@ -1,5 +1,6 @@
 package com.cityfruit.mozi.comman.component;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.cityfruit.mozi.comman.util.JsonUtil;
@@ -12,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -37,38 +39,36 @@ public class Runner implements CommandLineRunner {
         Map<String, Object> products = ZentaoUtil.getProducts();
         // 遍历产品列表，获取当日创建
         for (String productId : products.keySet()) {
-            // 统计未确认 BUG
+
+            // 统计未确认 BUG // 遍历 BUG 列表，更新并保存未确认的 BUG 列表
             log.info("[统计未确认……] Product ID: {}", productId);
-            String searchResult = ZentaoUtil.getSearchResult(ZentaoUtil.SEARCH_TODAY_OPENED_WITHOUT_CONFIRMED, ZentaoUtil.zentaoCookie, productId);
-            JSONArray jsonBugs = JSONObject.parseObject(searchResult).getJSONObject("data").getJSONArray("bugs");
-            // 遍历 BUG 列表，更新并保存未确认的 BUG 列表
-            for (int i = 0; i < jsonBugs.size(); i++) {
-                Bug bug = new Bug(jsonBugs.getJSONObject(i));
-                log.info("{}", bug);
-                Utils.updateQualityPointAndSave(jsonScore, bug, BugConst.ACTION_TYPE_OPEN, false);
-            }
-            // 统计确认 BUG 得分
+            execZentaoSearch(jsonScore, ZentaoUtil.SEARCH_TODAY_OPENED_WITHOUT_CONFIRMED, productId, BugConst.ACTION_TYPE_OPEN);
+
+            // 统计确认 BUG 得分 // 遍历 BUG 列表，更新并保存 QP 值
             log.info("[统计确认……] Product ID: {}", productId);
-            searchResult = ZentaoUtil.getSearchResult(ZentaoUtil.SEARCH_TODAY_OPENED_WITH_CONFIRMED, ZentaoUtil.zentaoCookie, productId);
-            jsonBugs = JSONObject.parseObject(searchResult).getJSONObject("data").getJSONArray("bugs");
-            // 遍历 BUG 列表，更新并保存 QP 值
-            for (int i = 0; i < jsonBugs.size(); i++) {
-                Bug bug = new Bug(jsonBugs.getJSONObject(i));
-                log.info("{}", bug);
-                Utils.updateQualityPointAndSave(jsonScore, bug, BugConst.ACTION_TYPE_CONFIRM, false);
-            }
-            // 统计关闭 BUG 得分
+            execZentaoSearch(jsonScore, ZentaoUtil.SEARCH_TODAY_OPENED_WITH_CONFIRMED, productId, BugConst.ACTION_TYPE_CONFIRM);
+
+            // 统计关闭 BUG 得分 // 遍历 BUG 列表，更新并保存 QP 值
             log.info("[统计关闭……] Product ID: {}", productId);
-            searchResult = ZentaoUtil.getSearchResult(ZentaoUtil.SEARCH_TODAY_CLOSED, ZentaoUtil.zentaoCookie, productId);
-            jsonBugs = JSONObject.parseObject(searchResult).getJSONObject("data").getJSONArray("bugs");
-            // 遍历 BUG 列表，更新并保存 QP 值
-            for (int i = 0; i < jsonBugs.size(); i++) {
-                Bug bug = new Bug(jsonBugs.getJSONObject(i));
-                log.info("{}", bug);
-                Utils.updateQualityPointAndSave(jsonScore, bug, BugConst.ACTION_TYPE_CLOSE, false);
-            }
+            execZentaoSearch(jsonScore, ZentaoUtil.SEARCH_TODAY_CLOSED, productId, BugConst.ACTION_TYPE_CLOSE);
+
         }
         log.info("[当日 Quality Point 获取情况统计完毕]");
+    }
+
+    private void execZentaoSearch(JSONObject jsonScore, Integer type, String productId, String actionType) {
+        String searchResult = ZentaoUtil.getSearchResult(type, ZentaoUtil.zentaoCookie, productId);
+        execUpdate(jsonScore, searchResult, actionType);
+    }
+
+    private void execUpdate(JSONObject jsonScore, String content, String type) {
+        String bugsStr = JSONObject.parseObject(content).getJSONObject("data").getJSONArray("bugs").toJSONString();
+        System.out.println(bugsStr);
+        List<Bug> bugs = JSON.parseArray(bugsStr, Bug.class);
+        for (Bug bug : bugs) {
+            log.info("{}", bug);
+            Utils.updateQualityPointAndSave(jsonScore, bug, type, false);
+        }
     }
 
 }
